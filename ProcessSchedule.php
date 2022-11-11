f76fe1393e7ed17bd5401011c9c223c08b965c31
Your Name <you@example.com>
2022-11-10 22:30:25 -0600

    Modified add, update, and delete functionality for employees' schedule by the Manager; Now fully functional.

--- ProcessSchedule.php
+++ ProcessSchedule.php
@@ -1,32 +1,19 @@
 <?php
 include 'SetSchedule.php';
-//include 'ServerConnection.php';
 
+//initialize variables the values of which are used as data for schedule table
 $workDays = '';
 $username = $_SESSION['username'];
-$employeeID = $_POST['EmployeeID'];
+$employeeID = $_POST['number'];
 $shift = $_POST['Shift'];
 $startTime = $_POST['starttime'];
 $endTime = $_POST['endtime'];
+$SchedulerID = '';
 
 If(isset($_POST)){
-    if($_POST['ProcessSchedule'] == 'Add Schedule'){
-        //echo 'You are adding schedule!';
 
-        //addSchedule($employeeID);
-    }
-    else if($_POST['ProcessSchedule'] == 'Update Schedule'){
-        //echo 'You are updating schedule';
-
-        //updateSchedule($employeeID);
-    }
-    else if($_POST['ProcessSchedule'] == 'Delete Schedule'){
-        //echo 'you are deleting schedule';
-
-       // deleteSchedule($employeeID);
-    }
-    //echo $_SESSION['username'];
-    
+    /*this formats the data for WorkDays column of Schedule table. The basic idea is to separate each days
+    by comma and treat it as a single varchar entry for the data*/
     $daysCount = 1;
     foreach($_POST['Work_Days_list'] as $selected){
         if($daysCount < count($_POST['Work_Days_list'])){
@@ -36,35 +23,80 @@ If(isset($_POST)){
             $workDays = $workDays . $selected;
         }
         
-        }
-    /*
-    echo 'Creator: '.$_SESSION['username']. "</br>";
-    echo 'EmployeeId: '.$employeeID."</br>";
-    echo 'Shift: '.$shift."</br>";
-    echo 'Start Time: '.$startTime. "</br>";
-    echo 'End Time: '.$endTime. "</br>";
-    echo 'Work Days: '.$workDays. "</br>";
-    */     
-}
-/*
-function addSchedule($employeeID){
-    /*
-    $sql = "SELECT "
-    $sql = "INSERT INTO schedule (UserID,SchedulerID,WorkDays,Shift,StartTime,EndTime)
-    VALUES ('$sanitized_FirstName', '$sanitized_LastName', '$sanitized_DOB', '$sanitized_SSN', '$sanitized_Address', '$sanitized_TelNum', '$sanitized_email')";
+    }
     
-    $result1 = mysqli_query($db, $sql1);
-    
-    return;
-}
-//to be completed
-function updateSchedule($employeeID){
-    return;
-}
-//to be completed
-function deleteSchedule($employeeID){
-    return;
+    //this captures the userID of the manager who has logged into the system
+    $sql = "SELECT UserID FROM Manager JOIN Login ON Manager.PII_ID = Login.PII_ID WHERE Login.UserName = '$username';";
+    $result = mysqli_query($db, $sql);
 
+    if(!$result){
+        die('Invalid query: '.mysqli_error());
+    }
+
+    $row = $result->fetch_assoc();
+    $SchedulerID = $row['UserID'];
+    //check if schedule table has userID that is equal to employeeID
+    $sql_check_schedule = "SELECT UserID FROM Schedule WHERE UserID = $employeeID;";
+    //Check if employee table has userID that is equal to employeeID
+    $sql_check_employee = "SELECT UserID FROM Employee WHERE UserID = $employeeID;";
+    $result_check_schedule = mysqli_query($db, $sql_check_schedule);
+    $result_check_employee = mysqli_query($db, $sql_check_employee);
+    $row_check_schedule = $result_check_schedule->fetch_assoc();
+    $row_check_employee = $result_check_employee->fetch_assoc();
+
+    if($_POST['ProcessSchedule'] == 'Add Schedule'){
+        //if data in Schedule table for selected employeeID already exist, display the following message.
+        if($row_check_schedule!=NULL){
+            echo "<Strong style = 'color: crimson'>".'Schedule for EmployeeID '.$employeeID. ' already exist! Data not added!'."</Strong>";
+        //if data in Schedule table for selected employeeID do not exist and data in Employee table for that employeeID exist, then insert data in the Schedule table.
+        }else if($row_check_schedule==NULL and $row_check_employee!=NULL){
+            $sql_insert = "INSERT INTO Schedule (UserID, SchedulerID, WorkDays, Shift, StartTime, EndTime)
+             VALUES ($employeeID, $SchedulerID, '$workDays', '$shift', '$startTime', '$endTime');";
+             $result = mysqli_query($db, $sql_insert);
+             if($result){
+                echo "<Strong>".'Schedule for EmployeeID '. $employeeID . ' added successfully!'."</Strong>";
+              }
+              else {
+                echo "<Strong style = 'color:crimson'>".'Schedule creation failed. Please try again!'."</Strong>";
+              }
+        }
+        //If no data for selected employeeID exist in Employee table, then display the following message.
+        else if($row_check_employee == NULL){
+            echo "<Strong style = 'color: red'>".'Please select the corret EmployeeID!'."</Strong>";
+            
+        }
+    }
+    else if($_POST['ProcessSchedule'] == 'Update Schedule'){
+        //if nothing for selected employeeID exist in Schedule table, then display this message.
+        if ($row_check_schedule == NULL){
+            echo "<Strong style = 'color: crimson'>".'Employee with EmployeeID '.$employeeID. ' not found in Schedule!'."</Strong>";
+
+        }else{
+            $sql_update = "UPDATE Schedule SET SchedulerID = $SchedulerID , WorkDays = '$workDays', Shift = '$shift', 
+            StartTime = '$startTime', EndTime = '$endTime' WHERE Schedule.UserID = $employeeID;";
+            $result_update = mysqli_query($db, $sql_update);
+            if($result_update){
+                echo "<Strong>".'Schedule for EmployeeID '. $employeeID . ' updated successfully!'."</Strong>";
+              }else{
+                echo "<Strong style = 'color: crimson'>".'Schedule update failed. Please try again!'."</Strong>";
+              }
+            }
+        }
+    
+    else if($_POST['ProcessSchedule'] == 'Delete Schedule'){
+        //if nothing for selected employeeID exist in Schedule table, then display this message.
+        if($row_check_schedule == NULL){
+            echo "<Strong style = 'color: crimson'>".'Employee with EmployeeID '.$employeeID. ' not found in Schedule! Delete failed!'."</Strong>";
+        }else{
+            $sql_delete = "DELETE FROM Schedule WHERE UserID = $employeeID;";
+            $result_delete = mysqli_query($db, $sql_delete);
+            if($result_delete){
+                echo "<Strong>".'Schedule for EmployeeID '. $employeeID . ' deleted successfully!'."</Strong>";
+            }else{
+                echo "<Strong style = 'color: crimson'>".'Schedule deletion failed. Please try again!'."</Strong>";
+            }
+        }
+    }
 }
-*/
+
 ?>
--- SetSchedule.php
+++ SetSchedule.php
@@ -6,67 +6,107 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Schedule</title>
 </head>
-<stye>
-
+<style>
+    .Submit {
+        background-color: #0066A2;
+        color: white;
+        padding: 08px 10px;
+        margin-right: 25px;
+        font-size: small;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+        float: left;
+    }
+    .Submit:hover {
+        background-color: #02c8db;
+    }
+    .Submit:active {
+        background-color: #88ef9e;
+    }
+    .number {
+        background-color: lightgray;
+        border:none;
+        padding: 04px 04px;
+        border-radius:4px;
+    }
+    .Shift {
+        background-color: lightgray;
+        border:none;
+        padding: 04px 04px;
+        border-radius:4px;
+    }
+    .time {
+        background-color: lightgray;
+        border:none;
+        padding: 04px 04px;
+        border-radius:4px;
+    }
+    .Checkbox {
+        background-color: lightgray;
+        border:none;
+        height:15px;
+        width:15px;
+    }
+    
 </style>
+
 <body>
     <div class = "left">
         <?php
-            //include 'ServerConnection.php';
             include 'EmployeeLookup.php';
         ?>
     </div>
     
     <div class = "right">
         <br>
-        <br>
         <center>
           <h2> Employee Schedule Processing</h2>
         </center>
         <hr>
         <br>
-        <br>
-        <form action="ProcessSchedule.php" method="POST">
+        <form action="ProcessSchedule.php" method = "POST">
             <div class="container">
-                  <label> EmployeeID :  </label>
-                  <input type="number" name="EmployeeID" placeholder="ID # from above " min="1"  required />
-                  <br>
-                  <br>
-                  <div>
-                  <label> Work Shift : </label>
-                        <select id = "Shift" name="Shift" width = "15">
-                              <option value="1st" name="First">1st</option>
-                              <option value="2nd" name="Second">2nd</option>
+
+                <label> EmployeeID :  </label>
+                <input type = "number" name = "number" class = "number" placeholder = "ID# " min = "1" required />
+                <br>
+                <br>
+                <div>
+                    <label> Work Shift : </label>
+                        <select id = "Shift" class = "Shift" name = "Shift" width = "15">
+                            <option value = "1st" name = "First">1st</option>
+                            <option value = "2nd" name = "Second">2nd</option>
                         </select>
-                  <br>
-                  <br>
-                  <label> Start Time : </label>
-                  <input type="time" name="starttime" />
-                  <label> End Time : </label>
-                  <input type="time" name="endtime" />
-                  </div>
-                  <br>
-                  <div>
-                        <label>Work Days : </label>
-                        <input type = "checkbox" name = "Work_Days_list[]" value = "M">
-                        <label for = "M">Monday</label>
-                        <input type = "checkbox" name = "Work_Days_list[]" value = "T">
-                        <label for = "T">Tuesday</label>
-                        <input type = "checkbox" name = "Work_Days_list[]" value = "W">
-                        <label for = "W">Wednesday</label>
-                        <input type = "checkbox" name = "Work_Days_list[]" value = "Th">
-                        <label for = "Th">Thursday</label>
-                        <input type = "checkbox" name = "Work_Days_list[]" value = "F">
-                        <label for = "F">Friday</label><br>
-                  <br>
-                  <input type="Submit" name="ProcessSchedule" value = "Add Schedule" >
-                  <input type="Submit" name="ProcessSchedule" value = "Update Schedule">
-                  <input type="Submit" name="ProcessSchedule" value = "Delete Schedule">
+                    <br>
+                    <br>
+                    <label> Start Time : </label>
+                        <input type = "time" class = "time" name = "starttime" value = "00:00:00"/>
+                    <label> End Time : </label>
+                        <input type = "time" class = "time" name = "endtime" value = "00:00:00"/>
+                </div>
+                <br>
+                <div>
+                    <label class = "workdays">Work Days : </label>
+                    <input type = "checkbox" class = "Checkbox" name = "Work_Days_list[]" value = "M" checked>
+                    <label for = "M">Monday</label>
+                    <input type = "checkbox" class = "Checkbox" name = "Work_Days_list[]" value = "T">
+                    <label for = "T">Tuesday</label>
+                    <input type = "checkbox" class = "Checkbox" name = "Work_Days_list[]" value = "W">
+                    <label for = "W">Wednesday</label>
+                    <input type = "checkbox" class = "Checkbox" name = "Work_Days_list[]" value = "Th">
+                    <label for = "Th">Thursday</label>
+                    <input type = "checkbox" class = "Checkbox" name = "Work_Days_list[]" value = "F">
+                    <label for = "F">Friday</label><br>
+                    <br>
+                    <input type = "Submit" class = "Submit" name = "ProcessSchedule" value = "Add Schedule" >
+                    <input type = "Submit" class = "Submit" name = "ProcessSchedule" value = "Update Schedule">
+                    <input type = "Submit" class = "Submit" name = "ProcessSchedule" value = "Delete Schedule">
+                </div>
+                <br>
+                <br>
             </div>
          </form>    
-        <br>
-        <br>
-      <div>
-      </div>
+    </div>
 </body>
 </html>
