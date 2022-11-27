8d7fb57fb3d3ecaabe52bd02572ea0a94804d1c7
Sapana <kunwar@maile.com>
2022-11-27 12:50:08 -0600

    Added footer

--- EmployeeLanding.php
+++ EmployeeLanding.php
@@ -35,46 +35,45 @@ require_once "ServerConnection.php";
             </div>
 
             <div class ="content">
-    <div class info ="info">
-                <h2> Employee Management System!</h2>
-                <strong >Welcome Employees!</strong> <br>
-                <p>Now more than ever users want unified platforms, this projects name E.M.S is designed to be an advanced employee management system, whichever company that uses this software will have the ability to manage employees’ hours, and PTO, and allow the employee to view roles and responsibilities, and what benefits does the company provide for them. This website will enable department managers to access employee information and view their hours, PTO requests, and feedback all in one place. E.M.S will also allow Human Resources administrators access to all employee data and their feedback to the company. 
-E.M.S requirements came about from company needs for such systems, it took what other similar systems did and where they lacked and enhanced them where possible</p> <br>
-<h3 style="color:#F47174;"> Latest News and Announcements!</h3> 
+                <div class info ="info">
+                    <h2> Employee Management System!</h2>
+                    <strong >Welcome Employees!</strong> <br>
+                    <p>Now more than ever users want unified platforms, this projects name E.M.S is designed to be an advanced employee management system, whichever company that uses this software will have the ability to manage employees’ hours, and PTO, and allow the employee to view roles and responsibilities, and what benefits does the company provide for them. This website will enable department managers to access employee information and view their hours, PTO requests, and feedback all in one place. E.M.S will also allow Human Resources administrators access to all employee data and their feedback to the company. 
+                    E.M.S requirements came about from company needs for such systems, it took what other similar systems did and where they lacked and enhanced them where possible</p> <br>
+                    <h3 style="color:#F47174;"> Latest News and Announcements!</h3> 
 
-            <div class="footer-container">
-    <div class="footer">
-        <div class="footer-heading footer-1">
-            <h2>About US</h2>
-            <a href="AboutUs.html">About</a>
-        </div>
-        <div class="footer-heading footer-2">
-            <h2>Contact US</h2>
-            <a href="Contactus.html">Contact</a>
-        </div>
-       
-    </div>
-   </div>
-</section>
 
-                <?php
-            include_once 'ServerConnection.php';
+                        <?php
+                            include_once 'ServerConnection.php';
 
-            $sql = "SELECT Communication From Communication;";
+                            $sql = "SELECT Communication From Communication;";
 
-            $result = mysqli_query($db, $sql);
-            if(!$result){
-                die("Invalid query: " .mysqli_error());
-            }
-            while($row = $result->fetch_assoc()){
-                echo "<tr>
-                <td> $row[Communication] </td>
-                
-            </tr>";
-            }
-            
-            ?>
+                            $result = mysqli_query($db, $sql);
+                            if(!$result){
+                                die("Invalid query: " .mysqli_error());
+                            }
+                            while($row = $result->fetch_assoc()){
+                                echo "<tr>
+                                <td> $row[Communication] </td>
+                                
+                            </tr>";
+                            }
+                        
+                        ?>
+                    <div class="footer-container">
+                        <div class="footer">
+                            <div class="footer-heading footer-1">
+                                <h2>About US</h2>
+                                <a href="AboutUs.html">About</a>
+                            </div>
+                            <div class="footer-heading footer-2">
+                                <h2>Contact US</h2>
+                                <a href="Contactus.html">Contact</a>
+                            </div>
 
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     </body>
