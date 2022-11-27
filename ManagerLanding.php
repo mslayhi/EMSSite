eebccd355c2743f6f272b125f3753416322f3054
Iiman1 <112735934+Iiman1@users.noreply.github.com>
2022-11-27 12:06:09 -0600

    Update ManagerLanding.php

--- ManagerLanding.php
+++ ManagerLanding.php
@@ -1,4 +1,6 @@
-
+<?php
+require_once "ServerConnection.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -36,10 +38,31 @@
             </div>
 
             <div class ="content">
-                    <div class info ="info">
+                   <div class info ="info">
                 <h2> Employee Management System!</h2>
                 <strong >Welcome Management!</strong>
-                <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, nostrum inventore aperiam perspiciatis quas totam autem adipisci esse. Incidunt at corporis repellat neque, quaerat qui cum! Quibusdam aperiam accusamus accusantium! Impedit, perspiciatis quam. Nulla temporibus aliquam corporis recusandae obcaecati ex nostrum officiis nesciunt, reiciendis facere, consectetur quos sed dolorum? Voluptatem quas velit quo corporis, distinctio ad consequuntur omnis temporibus mollitia perspiciatis veniam minus, unde possimus rerum. Quia doloremque sit, dolorem ipsam odio aspernatur totam ex nostrum error nihil neque vel. Voluptatem iure quisquam accusantium quam eaque laudantium quis cumque, incidunt ab. Magnam eos dolore, recusandae blanditiis necessitatibus neque. Qui, ipsam.</p>
+                <p>Now more than ever users want unified platforms, this projects name E.M.S is designed to be an advanced employee management system, whichever company that uses this software will have the ability to manage employees’ hours, and PTO, and allow the employee to view roles and responsibilities, and what benefits does the company provide for them. This website will enable department managers to access employee information and view their hours, PTO requests, and feedback all in one place. E.M.S will also allow Human Resources administrators access to all employee data and their feedback to the company. 
+E.M.S requirements came about from company needs for such systems, it took what other similar systems did and where they lacked and enhanced them where possible</p> <br>
+<h3 style="color:#F47174;"> Latest News and Announcements!</h3> 
+
+
+                <?php
+            include_once 'ServerConnection.php';
+
+            $sql = "SELECT Communication From Communication;";
+
+            $result = mysqli_query($db, $sql);
+            if(!$result){
+                die("Invalid query: " .mysqli_error());
+            }
+            while($row = $result->fetch_assoc()){
+                echo "<tr>
+                <td> $row[Communication] </td>
+                
+            </tr>";
+            }
+            
+            ?>
             </div>
         </section>
     </body>
