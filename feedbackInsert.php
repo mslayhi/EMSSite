2baf81250d0a2f32a213da99aa03865cd55f2740
Your Name <you@example.com>
2022-11-26 21:44:24 -0600

    Modified feedbackinsert file so that providerId can be captured for the feedback.

--- feedbackInsert.php
+++ feedbackInsert.php
@@ -2,21 +2,25 @@
 include_once 'ServerConnection.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST')
 {    
-    //  $FirstName = trim($_POST['FirstName']);
-    //  $LastName = trim($_POST['LastName']);
-    //  $UserID = trim($_POST['UserID']);
      $FeedBack = trim($_POST['FeedBack']);
+     $username = $_SESSION['username'];
+     $ProviderID = 0;
 
+     //get PII_ID of whoever has logged into the system
+     $sql_get_PiiID = "SELECT PII_ID FROM Login WHERE UserName = '$username';";
+     $result_get_PiiID = mysqli_query($db, $sql_get_PiiID);
+     if(!$result_get_PiiID){
+      die("Invalid query: ".mysqli_error());
+    }
+    $row_get_PiiID = $result_get_PiiID->fetch_assoc();
+    $ProviderID = (int) $row_get_PiiID['PII_ID'];
 
-    //  $sanitized_FirstName = mysqli_real_escape_string($conn, $FirstName);
-    //  $sanitized_LastName = mysqli_real_escape_string($conn, $LastName);
-    //  $sanitized_UserID = mysqli_real_escape_string($conn, $UserID);
      $sanitized_FeedBack = mysqli_real_escape_string($db, $FeedBack);
 
 
     if (isset($sanitized_FeedBack)) {
-      
-         $sql = "INSERT INTO feedback (FeedBack) VALUES ('$sanitized_FeedBack')";
+        //insert data into feedback table
+         $sql = "INSERT INTO feedback (ProviderID, FeedBack) VALUES ($ProviderID, '$sanitized_FeedBack');";
 		 
 		 // Write query and get result
 		 $result = mysqli_query($db, $sql);
