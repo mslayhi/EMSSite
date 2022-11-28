05e69251369d34575c73edce653d10ddad628ad4
Your Name <you@example.com>
2022-11-28 16:05:27 -0600

    Changed background color to match with Landing page.

--- EmployeeLookup.php
+++ EmployeeLookup.php
@@ -5,30 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EmployeeInfo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-</head>
-<body style="margin:100px 600px 100px 200px;">
-<button type="button" class="button" onclick="location.href='RedirectLandingPage.php'">
-        <span class="">Home Page</span>
-        <span class="">
-            <ion-icon name="main-page"></ion-icon>
-        </span>
-    </button>
-    <center>
-        <h2>Employee Information</h2>
-    </center>
-    <hr>
-    <br>
-
-    <table class="table" style = "border: 1px solid black">
-        <thead class="table-header" style="background-color: lightgreen;">
-            <tr>
-                <th>EmployeeID</th>
-                <th>FirstName</th>
-                <th>LastName</th>
-            </tr>
-        </thead>
-        <tbody>
-            <style>
+    <style>
+        .body{
+            background-color: #f1d1bc;
+            margin: 100px 400px 100px 200px;
+        }
           .button {
             background-color: #0066A2;
             color: white;
@@ -47,7 +28,47 @@
           .button:active {
             background-color: #88ef9e;
           }
+          .table{
+            border:1 px solid black;
+          }
+          .table-header{
+            background-color:lightgreen;
+            text-align: center;
+          }
+          .table-data{
+            text-align: center;
+          }
+          .table-row:hover{
+            background-color: #F0DFF2;
+            color:crimson;
+            cursor:pointer;
+            transition:0.3s;
+          }
         </style>
+</head>
+<body class = "body">
+<button type="button" class="button" onclick="location.href='RedirectLandingPage.php'">
+        <span class="">Home Page</span>
+        <span class="">
+            <ion-icon name="main-page"></ion-icon>
+        </span>
+    </button>
+    <center>
+        <h2>Employee Information</h2>
+    </center>
+    <hr>
+    <br>
+
+    <table class="table">
+        <thead class="table-header" style="background-color: lightgreen;">
+            <tr>
+                <th>EmployeeID</th>
+                <th>FirstName</th>
+                <th>LastName</th>
+            </tr>
+        </thead>
+        <tbody>
+           
             <?php
             include_once 'ServerConnection.php';
 
@@ -59,10 +80,10 @@
                 die("Invalid query: " .mysqli_error());
             }
             while($row = $result->fetch_assoc()){
-                echo "<tr>
-                <td> $row[UserID] </td>
-                <td> $row[FirstName] </td>
-                <td> $row[LastName] </td>
+                echo "<tr class = 'table-row'>
+                <td class = 'table-data'> $row[UserID] </td>
+                <td class = 'table-data'> $row[FirstName] </td>
+                <td class = 'table-data'> $row[LastName] </td>
             </tr>";
             }
             
